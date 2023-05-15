128a2f152d131cd4a43cd0e3a3206ed2998dc03e
amankumarcu <100796890+amankumarcu@users.noreply.github.com>
2023-05-16 03:24:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,61 @@
+# pip install streamlit fbprophet yfinance plotly
 import streamlit as st
 from datetime import date
 
-import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
+import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
 
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
+
 st.title('Stock Forecast App')
 
-# Set up Alpha Vantage API key
-API_KEY = "02UFWIM2I3UK5Y9N"
-
-# Define stocks and time period
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
-# Load data from Alpha Vantage API
-ts = TimeSeries(key=API_KEY, output_format='pandas')
-data, meta_data = ts.get_daily(symbol=selected_stock, outputsize='full')
-data.reset_index(inplace=True)
-data.columns = ['ds', 'y']
-data['ds'] = pd.to_datetime(data['ds'])
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+	
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
 
 st.subheader('Raw data')
 st.write(data.tail())
 
 # Plot raw data
 def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['ds'], y=data['y'], name="stock_close"))
-    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
-    
+	fig = go.Figure()
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+	fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+	fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+	st.plotly_chart(fig)
+	
 plot_raw_data()
 
 # Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
+
 m = Prophet()
-m.fit(data)
+m.fit(df_train)
 future = m.make_future_dataframe(periods=period)
 forecast = m.predict(future)
 
 # Show and plot forecast
 st.subheader('Forecast data')
 st.write(forecast.tail())
-
+    
 st.write(f'Forecast plot for {n_years} years')
 fig1 = plot_plotly(m, forecast)
 st.plotly_chart(fig1)
