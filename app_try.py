fc79f045fea68450b06a4083c21e461af651261c
amankumarcu <100796890+amankumarcu@users.noreply.github.com>
2023-05-16 11:33:38 +0530

    Update app_try.py

--- app_try.py
+++ app_try.py
@@ -1,60 +1,64 @@
 import streamlit as st
 from datetime import date
 
-import pandas as pd
-from alpha_vantage.timeseries import TimeSeries
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
-from sklearn.model_selection import train_test_split
+import yfinance as yf
+from fbprophet import Prophet
+from fbprophet.plot import plot_plotly
+from plotly import graph_objs as go
+
+START = "2015-01-01"
+TODAY = date.today().strftime("%Y-%m-%d")
 
 st.title('Stock Forecast App')
 
-# Set up Alpha Vantage API key
-API_KEY = "02UFWIM2I3UK5Y9N"
-
-# Define stocks and time period
 stocks = ('GOOG', 'AAPL', 'MSFT', 'GME')
 selected_stock = st.selectbox('Select dataset for prediction', stocks)
+
 n_years = st.slider('Years of prediction:', 1, 4)
 period = n_years * 365
 
-# Load data from Alpha Vantage API
-ts = TimeSeries(key=API_KEY, output_format='pandas')
-data, meta_data = ts.get_daily(symbol=selected_stock, outputsize='full')
-data.reset_index(inplace=True)
-data.columns = ['ds', 'y']
-data['ds'] = pd.to_datetime(data['ds'])
+
+@st.cache
+def load_data(ticker):
+    data = yf.download(ticker, START, TODAY)
+    data.reset_index(inplace=True)
+    return data
+
+
+data_load_state = st.text('Loading data...')
+data = load_data(selected_stock)
+data_load_state.text('Loading data... done!')
 
 st.subheader('Raw data')
 st.write(data.tail())
 
-# Split data into training and testing sets
-train_data, test_data = train_test_split(data, test_size=0.2, shuffle=False)
+# Plot raw data
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name="stock_open"))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name="stock_close"))
+    fig.layout.update(title_text='Time Series data with Rangeslider', xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
 
-# Create linear regression model and fit it to the training data
-lr = LinearRegression()
-lr.fit(train_data[['ds']], train_data[['y']])
+plot_raw_data()
 
-# Predict on the test set
-y_pred = lr.predict(test_data[['ds']])
+# Predict forecast with Prophet.
+df_train = data[['Date','Close']]
+df_train = df_train.rename(columns={"Date": "ds", "Close": "y"})
 
-# Calculate the root mean squared error (RMSE) of the model
-rmse = mean_squared_error(test_data[['y']], y_pred, squared=False)
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
 
-st.subheader('Test set prediction')
-st.write(test_data)
-st.write(f'RMSE: {rmse:.2f}')
-
-# Predict future values
-future_dates = pd.date_range(start=data['ds'].iloc[-1], periods=period, freq='D')[1:]
-future_data = pd.DataFrame({'ds': future_dates})
-
-y_pred = lr.predict(future_data[['ds']])
-forecast = pd.DataFrame({'ds': future_dates, 'y': y_pred})
-
+# Show and plot forecast
 st.subheader('Forecast data')
 st.write(forecast.tail())
 
 st.write(f'Forecast plot for {n_years} years')
-fig = px.line(forecast, x='ds', y='y', title=f'{selected_stock} stock price forecast')
-st.plotly_chart(fig)
+fig1 = plot_plotly(m, forecast)
+st.plotly_chart(fig1)
+
+st.write("Forecast components")
+fig2 = m.plot_components(forecast)
+st.write(fig2)
